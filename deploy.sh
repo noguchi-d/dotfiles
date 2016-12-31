ac929ef9b4edbc99898183923440b3116d492263
d_noguchi <daisuke.noguchi0107@gmail.com>
2016-12-31 11:56:17 +0900

    除外対象ファイルを調整

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,10 @@ file_list=`find ${DOT_DIRECTORY} -maxdepth 2 -type f`
 
 for f in $file_list
 do
-  if `echo dirname ${f}` | grep -q '.git'; then
+  if `echo dirname ${f}` | grep -q 'dotfiles/.git'; then
     continue
   fi
+  [[ `basename ${f}` = 'README.md' ]] && continue
   [[ `basename ${f}` = $0 ]] && continue
 
   f_name=`basename ${f}`
